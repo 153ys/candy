b34337b77d82e30f6dc635e47f24f271aa2bde90
153ys <daphnekao888@gmail.com>
2025-10-30 16:04:53 +0800

    更新sort寫法

--- 1027.js
+++ 1027.js
@@ -6,28 +6,8 @@
 //   [23, 71, 33, 82, 1] 印出 24
 
 function sumOfSmallestValues(arr) {
-  //假設前面兩個是最小值
-  let min1 = arr[0];
-  let min2 = arr[1];
-
-  //如果第二個比較小就往前移
-  if (min2 < min1) {
-    let temp = min2;
-    min1 = min2;
-    min2 = temp;
-  }
-
-  //比較第三個值
-  for (let i = 2; i <= arr.length; i++) {
-    let num = arr[i];
-    if (num < min1) {
-      min2 = min1;
-      min1 = num;
-    } else if (num < min2) {
-      min2 = num;
-    }
-  }
-  return min1 + min2;
+  arr.sort((a, b) => a - b);
+  return arr[0] + arr[1];
 }
 
 const list1 = [19, 5, 42, 2, 77];
